1d77740adc2c5139b2e0a6d9ae2e76437cceb2b6
brahimrz <rzbrahim5@gmail.com>
2023-08-11 09:49:51 -0700

    if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, z);
 	else if (z == 0)
 		printf("Last digit of %d is %d and is O\n", n, z);
-	else if (z < 6 && z != 0)
+	else if (z < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, z);
 
 	return (0);
