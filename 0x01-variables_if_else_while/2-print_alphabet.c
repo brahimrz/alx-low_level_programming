48eb13617eb5a7eb946224ec879fe784a5cb95c0
brahimrz <rzbrahim5@gmail.com>
2023-08-11 10:21:42 -0700

    for

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char rz;
+
+	for (rz = 'a' ; rz >= 'z' rz++)
+		putchar(rz);
+		putchar('\n');
+	return (0);
+}
