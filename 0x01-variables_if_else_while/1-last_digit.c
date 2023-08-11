c451d0fb4238e08973e8f9910e607cdbd00b66a7
brahimrz <rzbrahim5@gmail.com>
2023-08-11 08:45:34 -0700

    if else

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+include <stdio.h>
+
+/**
+ * main - the last digit of the number
+ * Return:0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	return (0);
+}
