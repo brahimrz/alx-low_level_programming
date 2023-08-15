d6dc452848b3d5b5f81d4297fea2d8453293d89d
brahimrz <rzbrahim5@gmail.com>
2023-08-15 06:55:48 -0700

    func

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -  function that prints the alphabet, in lowercase
+ * Returne: 0
+ */
+void print_alphabet(void)
+{
+	char str[28] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+
+	for (i = 0; i <= 28; i++)
+		putchar(str[i]);
+}
+int main(void)
+{
+	print_alphabet();
+}
