7632b5855026c851a61cc35c11c982b5a95bf21d
brahimrz <rzbrahim5@gmail.com>
2023-08-16 08:48:21 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - print the integer value
+ * @n: input
+ * Return: return 0 always (Success)
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
