1ef66a3cf64cb003387d42375b6abb9f987b6623
brahimrz <rzbrahim5@gmail.com>
2023-11-13 12:59:22 -0800

    l

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase character
+ * @c: checks for alphabetic character.
+ * Return: 1 if c is a letter, lowercase or uppercase
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+}
